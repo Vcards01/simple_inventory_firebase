df8d54699a8e9bd19be21b10b174fa74a90cd73e
Victor Cardoso <vhcardoso30@gmail.com>
2024-11-01 15:15:40 -0300

    projeto base + configuracoes do flutterfire

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+import '/ui/itemslist.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Inventory',
+      themeMode: ThemeMode.system,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: new ItemList(),
+    );
+  }
+}
